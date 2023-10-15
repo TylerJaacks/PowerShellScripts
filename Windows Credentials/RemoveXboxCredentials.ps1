3b433370368aaf780fad6d185fabffa71a3ff632
Tyler Jaacks <TylerJaacks@outlook.com>
2023-10-14 21:20:04 -0500

    Added a script to remove Xbox Live related Windows Credentials.

---
+++ Windows Credentials/RemoveXboxCredentials.ps1
@@ -0,0 +1,7 @@
+﻿Import-Module -Name Manage-StoredCredentials
+
+$credentials = Manage-StoredCredentials -ShoCred -All
+
+foreach ($cred in $credentials |Where { $_.Target -like "*Xbl*" } ) {
+    Manage-StoredCredentials -DelCred -Target $cred.Target -CredType GENERIC
+}
